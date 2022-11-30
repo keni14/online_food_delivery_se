4458e19ccecac971bace110790c49bc8d9467f0c
keni14 <83412960+keni14@users.noreply.github.com>
2022-11-30 23:04:16 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>Project by Nidhi Shetty, Namrata Keni, Nevasini S, Mitali Narayan</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
